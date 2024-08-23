6afa8b18ffc5017841e6c24e09ae3c936452dcf5
ellllie-42 <160203817+ellllie-42@users.noreply.github.com>
2024-08-23 00:10:53 -0800

    whoops

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
+#include <fstream>
 
 #include "SeraBot.h"
 #include "python_interpreter.h"
 
-std::string bot_token = "";
-
 int main()
 {
-    SeraBot discord_test_01(bot_token);
+    SeraBot discord_test_01(token_from_file(std::ifstream("../Tokens/serabot.txt")));
 
     std::cout<<"Testing the integrated python interpreter."<<std::endl;
-    python_interpreter py_int("Test_001", "python -u -i");
-    std::string out_001;
-    py_int << "print(\"Hello World!\")";
-    py_int >> out_001;
+    python_interpreter py_int;
+    std::string out_001, out_002;
+    py_int << "print(\"Hello World!\")"; //literally all you have to do the interpreter class will do the rest
+    std::getline(py_int, out_001);
     std::cout << "Python Console 001: " << out_001 << std::endl;
+    py_int << "x = 2";
+    py_int << "y = 3";
+    py_int << "print('x + y =' + str(x+y))";
+    py_int << "print('Henlo Wormld')";
+    py_int << "print('i like trains')";
+    std::getline(py_int, out_002);
+    std::cout << "Python Console 001: " << out_002 << std::endl;
+    while(std::getline(py_int, out_001))
+    {
+        std::cout << "Python Console 001: " << out_001 << std::endl;
+    }
 
-
-    std::cout<<py_int.running()<<std::endl;
     if(py_int.running() == true)
+    //if(true != true)
     {
         discord_test_01.start(dpp::st_wait);
     }
